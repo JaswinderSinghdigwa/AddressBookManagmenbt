2067b0abcc27bd00f6ce25cf3cd9a0421d0e4098
Aman <you@example.com>
2021-09-25 19:02:22 +0530

    work on addressbook

---
+++ src/com/bl/addressbook/AddressBook.java
@@ -0,0 +1,24 @@
+package com.bl.addressbook;
+
+
+
+public class AddressBook {
+    public static void main(String[] args) {
+        String fname,lname,address,city,state,zip,phone;
+        System.out.println("Welcome To Address Book");
+        System.out.println("Enter First Name");
+        fname=InputUtil.getStringValue();
+        System.out.println("Enter Last Name");
+        lname=InputUtil.getStringValue();
+        System.out.println("Enter Address");
+        address=InputUtil.getStringValue();
+        System.out.println("Enter City");
+        city=InputUtil.getStringValue();
+        System.out.println("Enter State");
+        state=InputUtil.getStringValue();
+        System.out.println("Enter Zip");
+        zip=InputUtil.getStringValue();
+        System.out.println("Enter phone number");
+        phone=InputUtil.getStringValue();
+    }
+}
